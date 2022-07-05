efd6a6b640e84db65bf80cb70d6f19357be5b5d1
Chase Forestello <chase.forestello@gmail.com>
2022-07-04 20:16:01 -0500

    live site

--- main.js
+++ main.js
@@ -14,9 +14,10 @@ function newRoast () {
 }
 
 function renderCoffee(coffee) {
-    var html = `<div class="coffee col-12 col-sm-12 col-md-6 col-lg-6 col-xxl-6"><div class="fs-4 fw-bold">${coffee.name}</div><div class="fs-6 text-muted">${coffee.roast}</div></div>`
-
-    return html;
+    return `<div class="coffee col-12 col-sm-12 col-md-6 col-lg-6 col-xxl-6">
+                    <div class="fs-4 fw-bold">${coffee.name}</div>
+                    <div class="fs-6 text-muted">${coffee.roast}</div>
+                </div>`;
 }
 
 function renderCoffees(coffees) {
@@ -41,6 +42,8 @@ function updateCoffees(e) {
     coffees.forEach(function(coffee) {
 
         if (selectedRoast === coffee.roast) {
+            // Trying to add icons to each coffee after the roast.
+            // Maybe could use set values of roast and selected roast to filter without icons causing issue otherwise need new way for icons.
             filteredCoffees.push(coffee);
         }
     });
@@ -66,21 +69,22 @@ searchInput.addEventListener("keyup", (event) => {
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
+    {id: 1, name: 'Light City', roast: 'light <i class="light fa-solid fa-mug-hot"></i>'},
+    {id: 2, name: 'Half City', roast: 'light <i class="light fa-solid fa-mug-hot"></i>'},
+    {id: 3, name: 'Cinnamon', roast: 'light <i class="light fa-solid fa-mug-hot"></i>'},
+    {id: 4, name: 'City', roast: 'medium <i class="medium fa-solid fa-mug-hot"></i>'},
+    {id: 5, name: 'American', roast: 'medium <i class="medium fa-solid fa-mug-hot"></i>'},
+    {id: 6, name: 'Breakfast', roast: 'medium <i class="medium fa-solid fa-mug-hot"></i>'},
+    {id: 7, name: 'High', roast: 'dark <i class="dark fa-solid fa-mug-hot"></i>'},
+    {id: 8, name: 'Continental', roast: 'dark <i class="dark fa-solid fa-mug-hot"></i>'},
+    {id: 9, name: 'New Orleans', roast: 'dark <i class="dark fa-solid fa-mug-hot"></i>'},
+    {id: 10, name: 'European', roast: 'dark <i class="dark fa-solid fa-mug-hot"></i>'},
+    {id: 11, name: 'Espresso', roast: 'dark <i class="dark fa-solid fa-mug-hot"></i>'},
+    {id: 12, name: 'Viennese', roast: 'dark <i class="dark fa-solid fa-mug-hot"></i>'},
+    {id: 13, name: 'Italian', roast: 'dark <i class="dark fa-solid fa-mug-hot"></i>'},
+    {id: 14, name: 'French', roast: 'dark <i class="dark fa-solid fa-mug-hot"></i>'},
 ];
+// Remove the icons but keep the classes and try using CSS to add icons or at least style the roasts
 
 section.innerHTML = renderCoffees(coffees.reverse());
 
