c3edbe4280a119b8836dbb1bbc644cbd0f95e159
Chase Forestello <chase.forestello@gmail.com>
2022-07-04 23:04:04 -0500

    live site

--- main.js
+++ main.js
@@ -14,9 +14,8 @@ function newRoast () {
 }
 
 function renderCoffee(coffee) {
-    return `<div class="coffee col-12 col-sm-12 col-md-6 col-lg-6 col-xxl-6">
-                    <div class="fs-4 fw-bold">${coffee.name}</div>
-                    <div class="fs-6 text-muted">${coffee.roast}</div>
+    return `<div class="coffee col-12 col-sm-12 col-md-12 col-lg-6 col-xxl-6">
+                    <div class="card fs-4 fw-bold text-nowrap text-center p-3 m-2 b-1"><span>${coffee.name} ${coffee.roast}</span></div>
                 </div>`;
 }
 
@@ -41,7 +40,7 @@ function updateCoffees(e) {
 
     coffees.forEach(function(coffee) {
 
-        if (selectedRoast === coffee.roast) {
+        if (selectedRoast === coffee.id) {
             // Trying to add icons to each coffee after the roast.
             // Maybe could use set values of roast and selected roast to filter without icons causing issue otherwise need new way for icons.
             filteredCoffees.push(coffee);
@@ -69,20 +68,20 @@ searchInput.addEventListener("keyup", (event) => {
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
-    {id: 1, name: 'Light City', roast: 'light <i class="light fa-solid fa-mug-hot"></i>'},
-    {id: 2, name: 'Half City', roast: 'light <i class="light fa-solid fa-mug-hot"></i>'},
-    {id: 3, name: 'Cinnamon', roast: 'light <i class="light fa-solid fa-mug-hot"></i>'},
-    {id: 4, name: 'City', roast: 'medium <i class="medium fa-solid fa-mug-hot"></i>'},
-    {id: 5, name: 'American', roast: 'medium <i class="medium fa-solid fa-mug-hot"></i>'},
-    {id: 6, name: 'Breakfast', roast: 'medium <i class="medium fa-solid fa-mug-hot"></i>'},
-    {id: 7, name: 'High', roast: 'dark <i class="dark fa-solid fa-mug-hot"></i>'},
-    {id: 8, name: 'Continental', roast: 'dark <i class="dark fa-solid fa-mug-hot"></i>'},
-    {id: 9, name: 'New Orleans', roast: 'dark <i class="dark fa-solid fa-mug-hot"></i>'},
-    {id: 10, name: 'European', roast: 'dark <i class="dark fa-solid fa-mug-hot"></i>'},
-    {id: 11, name: 'Espresso', roast: 'dark <i class="dark fa-solid fa-mug-hot"></i>'},
-    {id: 12, name: 'Viennese', roast: 'dark <i class="dark fa-solid fa-mug-hot"></i>'},
-    {id: 13, name: 'Italian', roast: 'dark <i class="dark fa-solid fa-mug-hot"></i>'},
-    {id: 14, name: 'French', roast: 'dark <i class="dark fa-solid fa-mug-hot"></i>'},
+    {id: 'light', name: 'Light City', roast: '<i class="light fa-solid fa-mug-hot"></i>'},
+    {id: 'light', name: 'Half City', roast: '<i class="light fa-solid fa-mug-hot"></i>'},
+    {id: 'light', name: 'Cinnamon', roast: '<i class="light fa-solid fa-mug-hot"></i>'},
+    {id: 'medium', name: 'City', roast: '<i class="medium fa-solid fa-mug-hot"></i>'},
+    {id: 'medium', name: 'American', roast: '<i class="medium fa-solid fa-mug-hot"></i>'},
+    {id: 'medium', name: 'Breakfast', roast: '<i class="medium fa-solid fa-mug-hot"></i>'},
+    {id: 'dark', name: 'High', roast: ' <i class="dark fa-solid fa-mug-hot"></i>'},
+    {id: 'dark', name: 'Continental', roast: '<i class="dark fa-solid fa-mug-hot"></i>'},
+    {id: 'dark', name: 'New Orleans', roast: '<i class="dark fa-solid fa-mug-hot"></i>'},
+    {id: 'dark', name: 'European', roast: '<i class="dark fa-solid fa-mug-hot"></i>'},
+    {id: 'dark', name: 'Espresso', roast: '<i class="dark fa-solid fa-mug-hot"></i>'},
+    {id: 'dark', name: 'Viennese', roast: '<i class="dark fa-solid fa-mug-hot"></i>'},
+    {id: 'dark', name: 'Italian', roast: '<i class="dark fa-solid fa-mug-hot"></i>'},
+    {id: 'dark', name: 'French', roast: '<i class="dark fa-solid fa-mug-hot"></i>'},
 ];
 // Remove the icons but keep the classes and try using CSS to add icons or at least style the roasts
 
